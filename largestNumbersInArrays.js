049fd298a9c2dfdb71b67dc78ece9b40e45aea5f
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-07-03 19:33:26 +0200

    finish largest-numbers-in-arrays.js
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

---
+++ largestNumbersInArrays.js
@@ -0,0 +1,24 @@
+function largestNumbersInArrays(arr) {
+  let finalArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    finalArray.push(Math.max(...arr[i]));
+  }
+  return finalArray;
+}
+
+console.log(
+  largestNumbersInArrays([
+    [17, 23, 25, 12],
+    [25, 7, 34, 48],
+    [4, -10, 18, 21],
+    [-72, -3, -17, -10],
+  ])
+);
+console.log(
+  largestNumbersInArrays([
+    [4, 9, 1, 3],
+    [13, 35, 18, 26],
+    [32, 35, 97, 39],
+    [1000000, 1001, 857, 1],
+  ])
+);
