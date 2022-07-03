a3b0c7837d0d509cb5eb04e367635c82e72c24ce
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-07-03 14:33:16 +0200

    finish reverse-string.js
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

---
+++ reverse-string.js
@@ -0,0 +1,10 @@
+function reverseString(str) {
+  let reverseString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString += str[i];
+  }
+  return reverseString;
+}
+
+console.log(reverseString('hello'));
+console.log(reverseString('Greetings from Earth'));
